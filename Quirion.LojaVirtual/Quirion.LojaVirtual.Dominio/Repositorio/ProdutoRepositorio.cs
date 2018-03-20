674e79f54c615c4a90055f5a53a3b7a8122f92d4
luizalveswolf <luiz.alves.wolf@gmail.com>
2018-03-20 16:12:26 -0300

    repositorio

---
+++ Quirion.LojaVirtual/Quirion.LojaVirtual.Dominio/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Quirion.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quirion.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutoRepositorio
+    {
+        private readonly EFDBContext _context = new EFDBContext();
+
+        public IEnumerable<Produto> Produto
+        {
+            get { return _context.Produtos; }
+        }
+
+        
+    }
+}
