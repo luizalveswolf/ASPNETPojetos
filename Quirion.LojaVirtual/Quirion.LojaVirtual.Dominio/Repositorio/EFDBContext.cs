052e7060d09d476466b5c18b7ca427cf3c8a4251
luizalveswolf <luiz.alves.wolf@gmail.com>
2018-03-20 15:40:36 -0300

    DBSet

---
+++ Quirion.LojaVirtual/Quirion.LojaVirtual.Dominio/Repositorio/EFDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Quirion.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quirion.LojaVirtual.Dominio.Repositorio
+{
+    public class EFDBContext: DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+
+
+
+}
